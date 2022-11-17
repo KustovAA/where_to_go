e7353813b62a839e8fe3d7e4bbd3430af807e34f
Андрей Кустов <andrey_kustov@MacBook-Pro-Andrej.local>
2022-11-18 01:49:37 +0300

    configure db

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
-
+import django_heroku
+import dj_database_url
 from environs import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,10 +84,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
@@ -135,3 +133,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
