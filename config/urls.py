9e54eb8a41269e9c0db416290d0e8ffd432d7382
Андрей Кустов <andrey_kustov@MacBook-Pro-Andrej.local>
2022-11-18 01:23:11 +0300

    add slash to url pattern

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,6 @@ from places import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('places/<int:id>', views.place, name='places'),
+    path('places/<int:id>/', views.place, name='places'),
     path('tinymce/', include('tinymce.urls')),
 ]
