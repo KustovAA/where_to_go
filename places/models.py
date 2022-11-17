5b3474f5eac7af4e390dc8e8b91289e6c12b7e7a
Андрей Кустов <andrey_kustov@MacBook-Pro-Andrej.local>
2022-11-18 02:02:29 +0300

    change fields in models

--- places/models.py
+++ places/models.py
@@ -4,10 +4,10 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=500, verbose_name='Название')
-    description_short = models.CharField(max_length=500, verbose_name='Описание(краткое)')
-    description_long = HTMLField(verbose_name='Описание(полное)')
-    lng = models.CharField(max_length=200, verbose_name='Долгота')
-    lat = models.CharField(max_length=200, verbose_name='Широта')
+    description_short = models.TextField(verbose_name='Описание(краткое)', null=True, blank=True)
+    description_long = HTMLField(verbose_name='Описание(полное)', null=True, blank=True)
+    lng = models.DecimalField(max_digits=10, decimal_places=8, verbose_name='Долгота')
+    lat = models.DecimalField(max_digits=10, decimal_places=8, verbose_name='Широта')
 
     def __str__(self):
         return self.title
