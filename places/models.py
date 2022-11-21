e5dbcb6d1daa622a3dad80bbd41188bf49396b38
Андрей Кустов <andrey_kustov@MacBook-Pro-Andrej.local>
2022-11-21 23:59:59 +0300

    make fields nonnullable

--- places/models.py
+++ places/models.py
@@ -4,8 +4,8 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=500, verbose_name='Название')
-    description_short = models.TextField(verbose_name='Описание(краткое)', null=True, blank=True)
-    description_long = HTMLField(verbose_name='Описание(полное)', null=True, blank=True)
+    description_short = models.TextField(verbose_name='Описание(краткое)', blank=True)
+    description_long = HTMLField(verbose_name='Описание(полное)', blank=True)
     lng = models.DecimalField(max_digits=10, decimal_places=8, verbose_name='Долгота')
     lat = models.DecimalField(max_digits=10, decimal_places=8, verbose_name='Широта')
 
